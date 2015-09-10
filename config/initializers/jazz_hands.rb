fa9e725b3551ef1ae52e04d2ffb6d1b04af0f62b
Leo Uzon <leo.uz@hotmail.com>
2015-09-10 19:20:28 -0300

    ...

---
+++ config/initializers/jazz_hands.rb
@@ -0,0 +1,10 @@
+if defined?(JazzHands)  
+  JazzHands.enable_syntax_highlighting_as_you_type!
+end
+
+if defined?(PryDebugger)
+  Pry.commands.alias_command 'c', 'continue'
+  Pry.commands.alias_command 's', 'step'
+  Pry.commands.alias_command 'n', 'next'
+  Pry.commands.alias_command 'f', 'finish'
+end
--- lib/cms_base/version.rb
+++ lib/cms_base/version.rb
@@ -1,3 +1,3 @@
 module CmsBase
-  VERSION = "1.0.0"
+  VERSION = "1.0.1"
 end
