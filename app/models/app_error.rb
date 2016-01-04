745906dff1199470604396c3a034b6de36f879e6
Leo Uzon <leo.uz86@gmail.com>
2016-01-04 20:02:59 -0200

    ...

--- app/models/app_error.rb
+++ app/models/app_error.rb
@@ -1,6 +1,26 @@
 class AppError < ActiveRecord::Base
   attr_accessible :where, :backtrace, :message
 
+  bs_admin do |c|
+    c.can [:view]
+    c.fields do |f|
+      f.string :where
+      f.string :message
+      f.text :backtrace
+    end
+
+    c.title_field :where
+    c.index_fields [:where, :message, :created_at]
+
+    c.sort 'created_at DESC'
+    c.filter :string, :where
+    c.filter :string, :message
+
+    c.populate_batch_count 5..20
+
+    c.paginate 50
+  end
+
   def self.create_from_exception where, exception
     AppError.create where: where, message: exception.message, backtrace: exception.backtrace
   end
--- lib/cms_base/version.rb
+++ lib/cms_base/version.rb
@@ -1,3 +1,3 @@
 module CmsBase
-  VERSION = "1.0.6"
+  VERSION = "1.0.7"
 end
